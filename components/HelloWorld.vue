3346b44439f94f0090906146a46cc229a00d9006
gbendjaf <ghaisbendjafar@gmail.com>
2023-10-28 21:43:08 +0200

    [HelloWorld] Testing purpose

--- components/HelloWorld.vue
+++ components/HelloWorld.vue
@@ -4,7 +4,7 @@ const { myLayer } = useAppConfig()
 
 <template>
   <div>
-    <h1>Hello World!</h1>
+    <h1>Hello World from extended component!</h1>
     <pre>{{ myLayer }}</pre>
   </div>
 </template>
