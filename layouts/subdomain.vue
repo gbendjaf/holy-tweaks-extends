cdbaa1609cfb627402d010f1718fbb8a4cc3b44f
gbendjaf <ghaisbendjafar@gmail.com>
2023-11-15 18:04:04 +0100

    [layout] silly me

--- layouts/subdomain.vue
+++ layouts/subdomain.vue
@@ -7,11 +7,11 @@
 
 <style scoped lang="scss">
 .ht-l-page {
-  padding: 50px 60px;
+  padding: 30px;
   background-color: $grey-20;
   min-height: 100vh;
   @media only screen and (min-width: 768px) {
-    padding: 30px;
+    padding: 50px 60px;
   }
 }
 </style>
