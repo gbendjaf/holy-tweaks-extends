6a22f76a6ab5c802683a671babf3521aac6ac79d
gbendjaf <ghaisbendjafar@gmail.com>
2024-02-08 21:08:10 +0100

    [Icons] add log out icons

---
+++ components/icon/LogOutBold.vue
@@ -0,0 +1,6 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+  <path fill-rule="evenodd" d="M8 2.25A6.75 6.75 0 0 0 1.25 9v6a6.75 6.75 0 0 0 13.5 0v-2.25H22a.75.75 0 0 0 0-1.5h-7.25V9A6.75 6.75 0 0 0 8 2.25Zm6.75 9H10a.75.75 0 0 0 0 1.5h4.75v-1.5Z" clip-rule="evenodd"/>
+  <path fill-rule="evenodd" d="M19.09 8.47a.75.75 0 0 1 1.06 0l1.94 1.939a2.25 2.25 0 0 1 0 3.182l-1.94 1.94a.75.75 0 1 1-1.06-1.061l1.94-1.94a.75.75 0 0 0 0-1.06l-1.94-1.94a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd"/>
+</svg>
+</template>
---
+++ components/icon/LogOutLinear.vue
@@ -0,0 +1,7 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+  <path fill-rule="evenodd" d="M8 3.75A5.25 5.25 0 0 0 2.75 9v6a5.25 5.25 0 0 0 10.201 1.75.75.75 0 1 1 1.415.5A6.752 6.752 0 0 1 1.25 15V9a6.75 6.75 0 0 1 13.116-2.25.75.75 0 1 1-1.415.5A5.253 5.253 0 0 0 8 3.75Z" clip-rule="evenodd"/>
+  <path fill-rule="evenodd" d="M9.25 12a.75.75 0 0 1 .75-.75h12a.75.75 0 0 1 0 1.5H10a.75.75 0 0 1-.75-.75Z" clip-rule="evenodd"/>
+  <path fill-rule="evenodd" d="M19.09 8.47a.75.75 0 0 1 1.06 0l1.94 1.939a2.25 2.25 0 0 1 0 3.182l-1.94 1.94a.75.75 0 1 1-1.06-1.061l1.939-1.94a.75.75 0 0 0 0-1.06l-1.94-1.94a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd"/>
+</svg>
+</template>
