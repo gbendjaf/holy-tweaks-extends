45a19d11aba0686e9bbddc3f13b2191ed9f9a9fa
gbendjaf <ghaisbendjafar@gmail.com>
2024-03-12 15:58:52 +0100

    [Icons] add dots icon

---
+++ components/icon/DotsVertical.vue
@@ -0,0 +1,5 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+  <path fill-rule="evenodd" d="M12 7a1 1 0 0 1-1-1v-.001a1 1 0 1 1 2 0V6a1 1 0 0 1-1 1Zm0 6.001a1 1 0 0 1-1-1V12a1 1 0 1 1 2 0v.001a1 1 0 0 1-1 1Zm0 6.001a1 1 0 0 1-1-1v-.001a1 1 0 1 1 2 0v.001a1 1 0 0 1-1 1Z" clip-rule="evenodd"/>
+</svg>
+</template>
