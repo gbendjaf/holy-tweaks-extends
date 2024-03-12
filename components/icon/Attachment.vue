02679a2eebbd74db4888c7311c90b33e2c46cb31
gbendjaf <ghaisbendjafar@gmail.com>
2024-03-12 16:52:21 +0100

    [icons] add attachment icon

---
+++ components/icon/Attachment.vue
@@ -0,0 +1,6 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+  <path fill-rule="evenodd" d="M8.25 12a.75.75 0 0 1 .75-.75h6a.75.75 0 0 1 0 1.5H9a.75.75 0 0 1-.75-.75Z" clip-rule="evenodd"/>
+  <path fill-rule="evenodd" d="M1.25 12A7.75 7.75 0 0 1 9 4.25h.5a.75.75 0 0 1 0 1.5H9a6.25 6.25 0 1 0 0 12.5h.5a.75.75 0 0 1 0 1.5H9A7.75 7.75 0 0 1 1.25 12Zm12.5-7a.75.75 0 0 1 .75-.75h.5a7.75 7.75 0 0 1 0 15.5h-.5a.75.75 0 0 1 0-1.5h.5a6.25 6.25 0 1 0 0-12.5h-.5a.75.75 0 0 1-.75-.75Z" clip-rule="evenodd"/>
+</svg>
+</template>
