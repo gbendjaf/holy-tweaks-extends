2c625928e485cec82885c112f153f70cb54242d4
gbendjaf <ghaisbendjafar@gmail.com>
2024-03-14 23:37:31 +0100

    [icons] add pointer icon

---
+++ components/icon/PointerBold.vue
@@ -0,0 +1,5 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="12" height="8" fill="currentColor" viewBox="0 0 12 8">
+  <path d="m0 0 5.2 7.564c.4.581 1.2.581 1.6 0L12 0H0Z"/>
+</svg>
+</template>
