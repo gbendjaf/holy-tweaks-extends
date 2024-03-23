c1fb5d7287190f808c3f01e529066238f0e3fbbb
gbendjaf <ghaisbendjafar@gmail.com>
2024-03-24 00:07:50 +0100

    [Layout] ensure bg color of subdomain layout use bg color variable

--- layouts/subdomain.vue
+++ layouts/subdomain.vue
@@ -8,7 +8,7 @@
 <style scoped lang="scss">
 .ht-l-page {
   padding: 30px;
-  background-color: $grey-15;
+  background-color: $bg-page;
   min-height: 100vh;
   @media only screen and (min-width: 768px) {
     padding: 50px 60px;
