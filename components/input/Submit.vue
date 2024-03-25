e20f060109087ba645fb700b9f95b1acc79a82c7
gbendjaf <ghaisbendjafar@gmail.com>
2024-03-25 18:39:40 +0100

    [Submit] now handle is loading state

--- components/input/Submit.vue
+++ components/input/Submit.vue
@@ -3,14 +3,16 @@ interface Props {
   text: string,
   disabled?: boolean,
   size?: 'medium' | 'large',
-  type?: 'primary' | 'secondary'
+  type?: 'primary' | 'secondary',
+  isLoading: boolean,
 }
 
 const props = withDefaults(defineProps<Props>(), {
   text: 'Input submit',
   size: 'medium',
   type: 'primary',
-  disabled: false
+  disabled: false,
+  isLoading: false
 })
 
 function useClass () {
@@ -21,7 +23,9 @@ function useClass () {
   const inputClass = reactive({
     '--large': props.size === 'large',
     '--primary': isPrimary.value,
-    '--secondary': !isPrimary.value
+    '--secondary': !isPrimary.value,
+    '--disabled': props.disabled,
+    '--is-loading': props.isLoading
   })
 
   return {
@@ -33,58 +37,89 @@ const { inputClass } =  useClass()
 </script>
 
 <template>
-  <input
+  <label
+    for="submit"
+    class="ht-c-submit-btn"
     :class="inputClass"
-    type="submit"
-    :value="text"
-    :disabled="disabled"
+    tabindex="0"
   >
+    <p v-if="!props.isLoading">{{ text }}</p>
+    <IconLoaderHalfCIrcle v-else />
+    <input
+      id="submit"
+      type="submit"
+      :disabled="disabled"
+    >
+  </label>
 </template>
 
 <style scoped lang="scss">
-input {
-  @include action-medium;
+.ht-c-submit-btn {
   border-radius: 2px;
   padding: 8px;
   cursor: pointer;
   width: fit-content;
   transition: background-color 0.1s linear;
-  &:not(:disabled):hover {
+  p {
+    @include action-medium;
+  }
+  &:not(.--disabled):hover {
     background-color: $bg-hoveractive-hi;
   }
   &.--large {
-    @include action-large;
+    p {
+      @include action-large;
+    }
     padding: 8px 12px;
   }
   &.--primary {
     border: none;
     user-select: none;
     background-color: $bg-action;
-    color: $text-onaction;
-    &:disabled {
+    p {
+      color: $text-onaction;
+    }
+    &.--disabled {
       background-color: $bg-disabled; 
     }
-    &:not(:disabled):hover {
+    &:not(.--disabled):hover {
       background-color: $bg-hoveractive-hi;
+    }
+    &.--is-loading svg {
+      color: $text-onaction;
     }
   }
   &.--secondary {
-    color: $text-action;
     background-color: transparent;
     border: 1px solid $border-action-hi;
-    &:disabled {
+    p {
+      color: $text-action;
+    }
+    &.--disabled {
       border: 1px solid $border-disabled;
     }
-    &:not(:disabled):hover {
+    &:not(.--disabled):hover {
       background-color: $alpha-white-8;
     }
+    &.--is-loading svg {
+      color: $text-action;
+    }
   }
-  &:disabled {
-    color: $text-disabled;
+  &.--disabled {
+    p {
+      color: $text-disabled;
+    }
     cursor: default;
   }
   &:focus-visible {
     outline: 1px solid $border-active-hi;
   }
+  &.--is-loading {
+    display: flex;
+    align-items: center;
+  }
+}
+input {
+  display: none;
 }
 </style>
