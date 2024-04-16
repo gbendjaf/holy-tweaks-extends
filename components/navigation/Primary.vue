2c75a94333932663e93de29cd072b02e1adfd4b7
gbendjaf <ghaisbendjafar@gmail.com>
2024-04-16 20:01:35 +0200

    [navigation] add navigation link styled like primary btn

---
+++ components/navigation/Primary.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+interface Props {
+  link: string,
+  size: 'medium' | 'large'
+  target?: string,
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  target: '_self',
+})
+</script>
+
+<template>
+  <NuxtLink
+    class="ht-c-link-primary ht-c-primary-btn-style"
+    :class="{ '--large': size === 'large' }"
+    :to="link"
+    :target="target"
+  >
+    <slot></slot>
+  </NuxtLink>
+</template>
+
+<style lang="scss">
+@import '@/assets/styling/components/buttons.scss';
+.ht-c-link-primary {
+
+}
+</style>
